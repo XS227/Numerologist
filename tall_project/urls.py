15d03f4aaa3f5a7314f27d810fd19af18b46fe02
Khabat Setaei <30699680+XS227@users.noreply.github.com>
2025-10-09 18:27:03 +0330

    Remove compiled Persian message catalog

--- tall_project/urls.py
+++ tall_project/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.http import HttpRequest, HttpResponse
 from django.urls import include, path
 
-from tall_project.views import home
+from tall_project.views import home, static_page
 
 
 def health_check(_request: HttpRequest) -> HttpResponse:
@@ -15,6 +15,8 @@ urlpatterns = [
     path("cms/", include("wagtail.admin.urls")),
     path("documents/", include("wagtail.documents.urls")),
     path("intake/", include("intake.urls", namespace="intake")),
+    path("i18n/", include("django.conf.urls.i18n")),
     path("healthz/", health_check, name="health_check"),
     path("", home, name="home"),
+    path("<slug:slug>/", static_page, name="static_page"),
 ]
--- tall_project/views.py
+++ tall_project/views.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
-from django.http import HttpRequest, HttpResponse
+from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
+
+from .navigation import STATIC_PAGES
 
 from .forms import LiteCalculatorForm
 
@@ -16,3 +18,14 @@ def home(request: HttpRequest) -> HttpResponse:
         "result": result,
     }
     return render(request, "pages/home.html", context)
+
+
+def static_page(request: HttpRequest, slug: str) -> HttpResponse:
+    try:
+        page = STATIC_PAGES[slug]
+    except KeyError as exc:  # pragma: no cover - defensive branch
+        raise Http404 from exc
+    context = {
+        "page": page,
+    }
+    return render(request, page.template_name, context)
