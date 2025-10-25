144244458149a9e542c34fb5914bd6f2da4cd7a7
Sanskar Tiwari <sanskar.mk2@gmail.com>
2025-10-25 16:01:26 +0530

    new deploy files

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting server..."
-exec python manage.py runserver 0.0.0.0:8000
+exec python manage.py runserver 0.0.0.0:8005
